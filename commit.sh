72461b779e4fdc96b2876fd3b9e384cf3b177e17
Andrew Charneski <acharneski@gmail.com>
2019-12-15 20:06:40 -0800

    Update scripts

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in `./modules.sh`; do
+for f in `./modules.sh | tail -n +1`; do
     pushd $f;
     git add . && git commit -a -m "$1";
     popd;
--- gitall.sh
+++ gitall.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-for f in `./modules.sh`; do pushd $f;git $@;popd; done;
+for f in `./modules.sh | tail -n +1`; do
+    pushd $f;
+    git $@;
+    popd;
+done;
+git add . && git commit -a -m "$1";
